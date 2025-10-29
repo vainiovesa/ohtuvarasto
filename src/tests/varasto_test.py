13e46a945aa1a7cde326f6a1872aea7e365dd127
vainiovesa <vesa.vainio@helsinki.fi>
2025-10-29 16:36:30 +0200

    Restore test

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -11,7 +11,7 @@ class TestVarasto(unittest.TestCase):
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
-        self.assertAlmostEqual(self.varasto.saldo, 42)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_tilavuus_toimii(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
